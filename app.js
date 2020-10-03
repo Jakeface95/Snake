ac9d1d76c11bd462807fb885cf252f71511a87f0
Jake Williams <jake5195@hotmail.co.uk>
2020-10-04 00:19:23 +0100

    Snake moves

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170,
 main();
 
 function main() {
-    clearCanvas();
-    drawSnake();
+    setTimeout(function onTick() {
+        clear_board();
+        move_snake();
+        drawSnake();
+        main();
+    }, 100)
 }
 
 function clearCanvas() {
@@ -22,26 +26,31 @@ function clearCanvas() {
 }
 
 function drawSnakePart(snakePart) {  
-  snakeboard_ctx.fillStyle = 'lightblue';  
-  snakeboard_ctx.strokestyle = 'darkblue';
-  snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);  
-  snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);wa
+    snakeboard_ctx.fillStyle = 'lightblue';  
+    snakeboard_ctx.strokestyle = 'darkblue';
+    snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);  
+    snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);wa
 }
 
 function drawSnake() {  
-  snake.forEach(drawSnakePart);
+    snake.forEach(drawSnakePart);
 }
 
 function drawSnakePart(snakePart) {
+    snakeboard_ctx.fillStyle = snake_col;
+    snakeboard_ctx.strokestyle = snake_border;
+    snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
 
-    // Set the colour of the snake part
-snakeboard_ctx.fillStyle = snake_col;
-    // Set the border colour of the snake part
-snakeboard_ctx.strokestyle = snake_border;
-    // Draw a "filled" rectangle to represent the snake part at the coordinates
-    // the part is located
-snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-    // Draw a border around the snake part
-snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-    }
-    
+// Directional velocities
+let dx = 10;
+let dy = 0;
+
+function move_snake() {
+    // Create the new Snake's head
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    // Add the new head to the beginning of snake body
+    snake.unshift(head);
+    snake.pop();
+}
