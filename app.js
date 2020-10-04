15859562fb7444336e4df5d3512bc89b81e7215f
Jake Williams <jake5195@hotmail.co.uk>
2020-10-04 16:15:29 +0100

    Working game - No styling

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ let snake = [
     {x: 160, y: 200}
 ]
 
+let score = 0;
 // True if changing direction
 let changing_direction = false;
 // Horizontal velocity
+let food_x;
+let food_y;
 let dx = 10;
 // Vertical velocity
 let dy = 0;
@@ -24,14 +27,14 @@ const snakeboard = document.getElementById("gameCanvas");
 const snakeboard_ctx = snakeboard.getContext("2d");
 // Start game
 main();
+gen_food();
+
 
 document.addEventListener("keydown", change_direction);
 
 // main function called repeatedly to keep the game running
 function main() {
-
     if (has_game_ended()) return;
-
     changing_direction = false;
     setTimeout(function onTick() {
     clear_board();
@@ -76,7 +79,7 @@ function drawSnakePart(snakePart) {
 
 function has_game_ended() {
     for (let i = 4; i < snake.length; i++) {
-    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
     }
     const hitLeftWall = snake[0].x < 0;
     const hitRightWall = snake[0].x > snakeboard.width - 10;
@@ -92,50 +95,45 @@ function change_direction(event) {
     const DOWN_KEY = 40;
 
     if (changing_direction) return;
-    changing_direction = true;
-    const keyPressed = event.keyCode;
-    const goingUp = dy === -10;
-    const goingDown = dy === 10;
-    const goingRight = dx === 10;
-    const goingLeft = dx === -10;
+        changing_direction = true;
+        const keyPressed = event.keyCode;
+        const goingUp = dy === -10;
+        const goingDown = dy === 10;
+        const goingRight = dx === 10;
+        const goingLeft = dx === -10;
     if (keyPressed === LEFT_KEY && !goingRight) {
-    dx = -10;
-    dy = 0;
+        dx = -10;
+        dy = 0;
     }
     if (keyPressed === UP_KEY && !goingDown) {
-    dx = 0;
-    dy = -10;
+        dx = 0;
+        dy = -10;
     }
     if (keyPressed === RIGHT_KEY && !goingLeft) {
-    dx = 10;
-    dy = 0;
+        dx = 10;
+        dy = 0;
     }
     if (keyPressed === DOWN_KEY && !goingUp) {
-    dx = 0;
-    dy = 10;
+        dx = 0;
+        dy = 10;
     }
 }
 
-function move_snake() {
-    // Create the new Snake's head
-    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
-    // Add the new head to the beginning of snake body
-    snake.unshift(head);
-    snake.pop();
-}
+function random_food(min, max) {
+    return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+  }
 
-function random_Location_Food(min, max) {  
-   return Math.round((Math.random() * (max-min) + min) / 10) * 10;
-}
-
-function gen_food() {  
-   food_x = random_Location_Food(0, snakeboard.width - 10);
-   food_y = random_Location_Food(0, snakeboard.height - 10);
-   snake.forEach(function has_snake_eaten_food(part) {
-        const has_eaten = part.x == food_x && part.y == food_y;
-        if (has_eaten) gen_food();
+  function gen_food() {
+    // Generate a random number the food x-coordinate
+    food_x = random_food(0, snakeboard.width - 10);
+    // Generate a random number for the food y-coordinate
+    food_y = random_food(0, snakeboard.height - 10);
+    // if the new food location is where the snake currently is, generate a new food location
+    snake.forEach(function has_snake_eaten_food(part) {
+      const has_eaten = part.x == food_x && part.y == food_y;
+      if (has_eaten) gen_food();
     });
-}
+  }
 
 function drawFood() {
       snakeboard_ctx.fillStyle = 'lightgreen';
@@ -143,3 +141,22 @@ function drawFood() {
       snakeboard_ctx.fillRect(food_x, food_y, 10, 10);
       snakeboard_ctx.strokeRect(food_x, food_y, 10, 10);
 }
+
+function move_snake() {
+    // Create the new Snake's head
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    // Add the new head to the beginning of snake body
+    snake.unshift(head);
+    const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
+    if (has_eaten_food) {
+      // Increase score
+      score += 10;
+      // Display score on screen
+      document.getElementById('score').innerHTML = score;
+      // Generate new food location
+      gen_food();
+    } else {
+      // Remove the last part of snake body
+      snake.pop();
+    }
+  }
