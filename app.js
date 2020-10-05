5dd82f584503df4806a2444b0e1684fdd18d08ed
Jake Williams <jake5195@hotmail.co.uk>
2020-10-05 11:03:50 +0100

    changed snake

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const board_border = 'black';
 const board_background = "lightgrey";
-const snake_col = 'purple';
+const snake_col = 'lightblue';
 const snake_border = 'darkblue';
 const button = document.querySelector("#restartButton");
 
